83d30706ba85cb4306fb0370ce3f87e89bb9d13b
Jan Šafář <johny.safar@gmail.com>
2025-03-31 20:33:00 +0200

    fix(ACR): add login server to private DNS zones (#4)

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,4 +1,10 @@
-resource "azurerm_private_dns_zone" "acr-pen-dns-zone" {
+resource "azurerm_private_dns_zone" "acr-pen-login-dns-zone" {
+  name                = "privatelink.azurecr.io"
+  resource_group_name = data.azurerm_resource_group.net-rg.name
+}
+
+
+resource "azurerm_private_dns_zone" "acr-pen-data-dns-zone" {
   name                = "germanywestcentral.data.privatelink.azurecr.io"
   resource_group_name = data.azurerm_resource_group.net-rg.name
 }
@@ -16,15 +22,28 @@ resource "azurerm_private_dns_zone" "cdb-pen-dns-zone" {
 }
 
 
-resource "azurerm_private_dns_zone_virtual_network_link" "acr-pen-dns-zone-vnet-link" {
-  name                = "${var.APP_NAME}-${var.ENV}-acr-pen-dns-zone-vnet-link"
+resource "azurerm_private_dns_zone_virtual_network_link" "acr-pen-login-dns-zone-vnet-link" {
+  name                = "${var.APP_NAME}-${var.ENV}-acr-pen-login-dns-zone-vnet-link"
   resource_group_name = data.azurerm_resource_group.net-rg.name
 
-  private_dns_zone_name = azurerm_private_dns_zone.acr-pen-dns-zone.name
+  private_dns_zone_name = azurerm_private_dns_zone.acr-pen-login-dns-zone.name
   virtual_network_id    = data.azurerm_virtual_network.vnet.id
 
   depends_on = [
-    azurerm_private_dns_zone.acr-pen-dns-zone
+    azurerm_private_dns_zone.acr-pen-login-dns-zone
+  ]
+}
+
+
+resource "azurerm_private_dns_zone_virtual_network_link" "acr-pen-data-dns-zone-vnet-link" {
+  name                = "${var.APP_NAME}-${var.ENV}-acr-pen-data-dns-zone-vnet-link"
+  resource_group_name = data.azurerm_resource_group.net-rg.name
+
+  private_dns_zone_name = azurerm_private_dns_zone.acr-pen-data-dns-zone.name
+  virtual_network_id    = data.azurerm_virtual_network.vnet.id
+
+  depends_on = [
+    azurerm_private_dns_zone.acr-pen-data-dns-zone
   ]
 }
 
@@ -53,38 +72,4 @@ resource "azurerm_private_dns_zone_virtual_network_link" "cdb-pen-dns-zone-vnet-
     azurerm_private_dns_zone.cdb-pen-dns-zone
   ]
 }
-
-
-resource "azurerm_private_dns_a_record" "acr-pen-nic-dns-a-record" {
-  name                = azurerm_container_registry.acr.name
-  resource_group_name = data.azurerm_resource_group.net-rg.name
-
-  zone_name = azurerm_private_dns_zone.acr-pen-dns-zone.name
-  records   = [azurerm_private_endpoint.acr-pen.private_service_connection.0.private_ip_address]
-  ttl       = 10
-
-  depends_on = [
-    azurerm_container_registry.acr,
-    azurerm_private_dns_zone.acr-pen-dns-zone,
-    azurerm_private_endpoint.acr-pen
-  ]
-}
-
-
-resource "azurerm_private_dns_a_record" "aks-pen-nic-dns-a-record" {
-  name                = azurerm_kubernetes_cluster.aks.name
-  resource_group_name = data.azurerm_resource_group.net-rg.name
-
-  zone_name = azurerm_private_dns_zone.aks-pen-dns-zone.name
-  records   = [azurerm_private_endpoint.aks-pen.private_service_connection.0.private_ip_address]
-  ttl       = 10
-
-  depends_on = [
-    azurerm_kubernetes_cluster.aks,
-    azurerm_private_dns_zone.aks-pen-dns-zone,
-    azurerm_private_endpoint.aks-pen
-  ]
-}
-
-
 
