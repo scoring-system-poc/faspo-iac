04c0c059c0944eaab1f25be92c7fd46422a3afa9
Jan Šafář <johny.safar@gmail.com>
2025-04-01 20:58:32 +0200

    fix(AKS): assign control plane and nodepool separate UAMIs

--- terraform/rbac.tf
+++ terraform/rbac.tf
@@ -1,46 +1,57 @@
-resource "azurerm_role_assignment" "aks-network-rbac" {
-  principal_id         = azurerm_user_assigned_identity.aks-uami.principal_id
+resource "azurerm_role_assignment" "aks-cp-network-rbac" {
+  principal_id         = azurerm_user_assigned_identity.aks-cp-uami.principal_id
   scope                = data.azurerm_virtual_network.vnet.id
   role_definition_name = "Network Contributor"
 
   depends_on = [
-    azurerm_user_assigned_identity.aks-uami
+    azurerm_user_assigned_identity.aks-cp-uami
   ]
 }
 
 
-resource "azurerm_role_assignment" "aks-acr-rbac" {
-  principal_id         = azurerm_user_assigned_identity.aks-uami.principal_id
-  scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-
-  depends_on = [
-    azurerm_user_assigned_identity.aks-uami,
-    azurerm_container_registry.acr
-  ]
-}
-
-
-resource "azurerm_role_assignment" "aks-dns-read-rbac" {
-  principal_id         = azurerm_user_assigned_identity.aks-uami.principal_id
+resource "azurerm_role_assignment" "aks-cp-dns-read-rbac" {
+  principal_id         = azurerm_user_assigned_identity.aks-cp-uami.principal_id
   scope                = azurerm_private_dns_zone.aks-pen-dns-zone.id
   role_definition_name = "Reader"
 
   depends_on = [
-    azurerm_user_assigned_identity.aks-uami,
+    azurerm_user_assigned_identity.aks-cp-uami,
     azurerm_private_dns_zone.aks-pen-dns-zone
   ]
 }
 
 
-resource "azurerm_role_assignment" "aks-dns-write-rbac" {
-  principal_id         = azurerm_user_assigned_identity.aks-uami.principal_id
+resource "azurerm_role_assignment" "aks-cp-dns-write-rbac" {
+  principal_id         = azurerm_user_assigned_identity.aks-cp-uami.principal_id
   scope                = azurerm_private_dns_zone.aks-pen-dns-zone.id
   role_definition_name = "Private DNS Zone Contributor"
 
   depends_on = [
-    azurerm_user_assigned_identity.aks-uami,
+    azurerm_user_assigned_identity.aks-cp-uami,
     azurerm_private_dns_zone.aks-pen-dns-zone
   ]
 }
 
+
+resource "azurerm_role_assignment" "aks-cp-uami-rbac" {
+  principal_id         = azurerm_user_assigned_identity.aks-cp-uami.principal_id
+  scope                = azurerm_private_dns_zone.aks-pen-dns-zone.id
+  role_definition_name = "Managed Identity Operator"
+
+  depends_on = [
+    azurerm_user_assigned_identity.aks-cp-uami,
+    azurerm_private_dns_zone.aks-pen-dns-zone
+  ]
+}
+
+
+resource "azurerm_role_assignment" "aks-nodepool-acr-rbac" {
+  principal_id         = azurerm_user_assigned_identity.aks-nodepool-uami.principal_id
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+
+  depends_on = [
+    azurerm_user_assigned_identity.aks-nodepool-uami,
+    azurerm_container_registry.acr
+  ]
+}
--- terraform/uami.tf
+++ terraform/uami.tf
@@ -1,6 +1,15 @@
-resource "azurerm_user_assigned_identity" "aks-uami" {
-  name = "${var.APP_NAME}-${var.ENV}-aks-uami"
+resource "azurerm_user_assigned_identity" "aks-cp-uami" {
+  name = "${var.APP_NAME}-${var.ENV}-aks-cp-uami"
 
   resource_group_name = data.azurerm_resource_group.sec-rg.name
   location            = data.azurerm_resource_group.sec-rg.location
 }
+
+
+resource "azurerm_user_assigned_identity" "aks-nodepool-uami" {
+  name = "${var.APP_NAME}-${var.ENV}-aks-nodepool-uami"
+
+  resource_group_name = data.azurerm_resource_group.sec-rg.name
+  location            = data.azurerm_resource_group.sec-rg.location
+}
+
