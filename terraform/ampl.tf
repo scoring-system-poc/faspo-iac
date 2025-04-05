97d66fc7c363a712034fc59c40685c7811e1f8ec
Jan Šafář <johny.safar@gmail.com>
2025-04-05 12:59:11 +0200

    fix(MONITOR): APPI+LAW private integration

--- terraform/ampl.tf
+++ terraform/ampl.tf
@@ -1,38 +1,32 @@
-resource "azurerm_monitor_private_link_scope" "law-ampls" {
-  name                = "${var.APP_NAME}-${var.ENV}-law-ampls"
+resource "azurerm_monitor_private_link_scope" "monitor-ampls" {
+  name                = "${var.APP_NAME}-${var.ENV}-monitor-ampls"
   resource_group_name = data.azurerm_resource_group.net-rg.name
 }
 
 
-resource "azurerm_monitor_private_link_scope" "appi-ampls" {
-  name                = "${var.APP_NAME}-${var.ENV}-appi-ampls"
-  resource_group_name = data.azurerm_resource_group.net-rg.name
-}
-
-
-resource "azurerm_monitor_private_link_scoped_service" "law-ampls-link" {
-  name                = "${var.APP_NAME}-${var.ENV}-law-ampls-link"
+resource "azurerm_monitor_private_link_scoped_service" "monitor-ampls-law-link" {
+  name                = "${var.APP_NAME}-${var.ENV}-monitor-ampls-law-link"
   resource_group_name = data.azurerm_resource_group.net-rg.name
 
-  scope_name         = azurerm_monitor_private_link_scope.law-ampls.name
+  scope_name         = azurerm_monitor_private_link_scope.monitor-ampls.name
   linked_resource_id = azurerm_log_analytics_workspace.law.id
 
   depends_on = [
-    azurerm_monitor_private_link_scope.law-ampls,
+    azurerm_monitor_private_link_scope.monitor-ampls,
     azurerm_log_analytics_workspace.law
   ]
 }
 
 
-resource "azurerm_monitor_private_link_scoped_service" "appi-ampls-link" {
-  name                = "${var.APP_NAME}-${var.ENV}-appi-ampls-link"
+resource "azurerm_monitor_private_link_scoped_service" "monitor-ampls-appi-link" {
+  name                = "${var.APP_NAME}-${var.ENV}-monitor-ampls-appi-link"
   resource_group_name = data.azurerm_resource_group.net-rg.name
 
-  scope_name         = azurerm_monitor_private_link_scope.appi-ampls.name
+  scope_name         = azurerm_monitor_private_link_scope.monitor-ampls.name
   linked_resource_id = azurerm_application_insights.appi.id
 
   depends_on = [
-    azurerm_monitor_private_link_scope.appi-ampls,
+    azurerm_monitor_private_link_scope.monitor-ampls,
     azurerm_application_insights.appi
   ]
 }
