602ceb5c1faa9e8e621d327b16069f7b14e357e9
Jan Šafář <johny.safar@gmail.com>
2025-04-07 17:29:20 +0200

    fix(test-app): change FC subject (#22)

--- terraform/fc.tf
+++ terraform/fc.tf
@@ -1,16 +1,1 @@
-resource "azurerm_federated_identity_credential" "test-app-uami-fc" {
-  name                = "${var.APP_NAME}-${var.ENV}-test-app-uami-fc"
-  resource_group_name = data.azurerm_resource_group.sec-rg.name
-
-  parent_id = azurerm_user_assigned_identity.test-app-uami.id
-  subject   = "system:serviceaccount:default:${var.APP_NAME}-${var.ENV}-test-app-sa"
-
-  audience = ["api://AzureADTokenExchange"]
-  issuer   = azurerm_kubernetes_cluster.aks.oidc_issuer_url
-
-  depends_on = [
-    azurerm_user_assigned_identity.test-app-uami,
-    azurerm_kubernetes_cluster.aks
-  ]
-}
 
