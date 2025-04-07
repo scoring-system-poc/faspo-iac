05ef49b942d4f97b3b873505bc68ae77ae80bb54
Jan Šafář <johny.safar@gmail.com>
2025-04-07 13:09:50 +0200

    fix(ARGOCD): perform initial setup

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -4,47 +4,77 @@ resource "helm_release" "argocd" {
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
 
-  namespace = kubernetes_namespace.aks-argocd-ns.metadata.0.name
-  skip_crds = true
+  namespace        = "argocd"
+  create_namespace = true
 
   values = [
     yamlencode({
-      server = {
-        service = {
-          type = "ClusterIP"
+      crds = {
+        additionalLabels = {
+          "argocd.argoproj.io/managed-by" = "argocd"
         }
       }
+      configs = {
+        cm = {
+          "admin.enabled"                = "true" # only for testing
+          "application.instanceLabelKey" = "app.kubernetes.io/instance"
+          "timeout.reconciliation"       = "60s"
+        }
+        repositories = {
+          git = {
+            type          = "git"
+            name          = "${var.APP_NAME}-apps"
+            url           = "git@github.com:${var.PROJECT_NAME}/${var.APP_NAME}-apps.git"
+            sshPrivateKey = base64decode(var.ARGOCD_DEPLOY_KEY)
+          }
+          helm = {
+            type      = "helm"
+            name      = azurerm_container_registry.acr.name
+            url       = "oci://${azurerm_container_registry.acr.name}.azurecr.io/helm"
+            username  = "argocd"
+            password  = azurerm_container_registry_token_password.argocd-acr-token-pwd.password1.0.value
+            enableOCI = "true"
+          }
+        }
+      }
+      extraObjects = [
+        {
+          apiVersion = "argoproj.io/v1alpha1"
+          kind       = "Application"
+          metadata = {
+            name      = "${var.APP_NAME}-apps"
+            namespace = "argocd"
+            finalizers = [
+              "resources-finalizer.argocd.argoproj.io"
+            ]
+          }
+          spec = {
+            project = "default"
+            source = {
+              repoURL        = "git@github.com:${var.PROJECT_NAME}/${var.APP_NAME}-apps.git"
+              targetRevision = "env/${var.ENV}"
+              path           = "."
+            }
+            destination = {
+              server    = "https://kubernetes.default.svc"
+              namespace = "argocd"
+            }
+            syncPolicy = {
+              automated = {
+                prune    = true
+                selfHeal = true
+              }
+            }
+          }
+        }
+      ]
     })
   ]
 
   depends_on = [
     azurerm_kubernetes_cluster.aks,
-    kubernetes_namespace.aks-argocd-ns
+    azurerm_container_registry.acr,
+    azurerm_container_registry_token_password.argocd-acr-token-pwd
   ]
 }
-
-
-resource "kubernetes_secret" "ghb-apps-deploy-key" {
-  metadata {
-    name      = "${var.APP_NAME}-${var.ENV}-ghb-apps-deploy-key"
-    namespace = kubernetes_namespace.aks-argocd-ns.metadata.0.name
-    labels = {
-      "argocd.argoproj.io/secret-type" = "repository"
-    }
-  }
-
-  data = {
-    ssh-key = base64decode(var.ARGOCD_DEPLOY_KEY)
-  }
-
-  type = "Opaque"
-
-  depends_on = [
-    azurerm_kubernetes_cluster.aks,
-    kubernetes_namespace.aks-argocd-ns
-  ]
-}
-
-
-# TODO: ArgoCD project that monitor faspo-apps repo
 
