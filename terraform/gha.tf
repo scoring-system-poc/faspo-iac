28af46b21d31ae1e614b00dd87dc12737192625e
Jan Šafář <johny.safar@gmail.com>
2025-04-09 12:33:42 +0200

    feat(ONLINE-DATA-SERVICE): init resources (APP/SP, RBAC) (#27)

--- terraform/gha.tf
+++ terraform/gha.tf
@@ -1,3 +1,9 @@
+resource "azuread_application" "online-data-service-gha-app" {
+  display_name            = "${var.APP_NAME}-${var.ENV}-online-data-service-gha"
+  prevent_duplicate_names = true
+}
+
+
 resource "azuread_application" "batch-data-service-gha-app" {
   display_name            = "${var.APP_NAME}-${var.ENV}-batch-data-service-gha"
   prevent_duplicate_names = true
@@ -13,6 +19,12 @@ resource "azuread_application" "export-service-gha-app" {
 resource "azuread_application" "store-service-gha-app" {
   display_name            = "${var.APP_NAME}-${var.ENV}-store-service-gha"
   prevent_duplicate_names = true
+}
+
+
+resource "azuread_service_principal" "online-data-service-gha-sp" {
+  client_id  = azuread_application.online-data-service-gha-app.client_id
+  depends_on = [azuread_application.online-data-service-gha-app]
 }
 
 
